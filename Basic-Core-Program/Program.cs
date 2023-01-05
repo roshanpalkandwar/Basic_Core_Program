5ec4b30b94ee08657a2b4ebf0b2caca1db64c9c0
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-05 23:04:13 +0530

    [Roshan]Added:Check The Alphabate Vowel Or Consonat

--- Basic-Core-Program/Program.cs
+++ Basic-Core-Program/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Basic Core Program");
             Console.WriteLine("Enter your choice ");
             Console.WriteLine("------------------------------------");
-            Console.WriteLine("1 For: Flip a Coin Head and Tail.\t 2.Select Value is Leap Year Or not \t 3.Select Power of 2 Number\t 4.harmonic series\t 5.the prime factor \t6.Quotient and Remainder\t7.swap number\t8.check even & odd Number");
+            Console.WriteLine("1 For: Flip a Coin Head and Tail.\t 2.Select Value is Leap Year Or not \t 3.Select Power of 2 Number\t 4.harmonic series\t 5.the prime factor \t6.Quotient and Remainder\t7.swap number\t8.check even & odd Number\t9.check alphabet is vowel or consonant");
             int selection = Convert.ToInt32(Console.ReadLine());
 
             switch (selection)
@@ -43,6 +43,9 @@
                     break;
                 case 8:
                     NumberEvenOrOdd.number();
+                    break;
+                case 9:
+                    checkalphabet.VowelOrConsonant();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection.");
